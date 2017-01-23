86075a9e01278ef1b5d07241333c53303136ed5e
benschau <bchau3440@gmail.com>
2017-01-23 13:34:09 -0500

    more utilities (incomplete and otherwise)

---
+++ scripts/panel-custom/barfeeder.py
@@ -0,0 +1,21 @@
+#!/bin/python2
+
+def launcher():
+    return "LAUNCHER"
+
+def wmctrl():
+    return "WMCTRL"
+
+def clock():
+    return "CLOCK"
+
+def battery():
+    return "BATTERY"
+
+def date():
+    return "DATE"
+
+def wifi():
+    return "WIFI"
+
+print launcher() + " " + clock()  
